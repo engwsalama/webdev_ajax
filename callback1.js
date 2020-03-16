da00f6190dee4f5e26f23ca0c0b66ce108fe1632
engwsalama <eng.wsalama@gmail.com>
2020-03-16 19:39:01 +0200

    update

--- callback1.js
+++ callback1.js
@@ -3,22 +3,22 @@ const posts =[
     {'title':"Post Two","body":"This is Post Two"}
 ];
 
-function createPost(post,callback){
-    setTimeout(function(){
-        posts.push(post);
-        callback();
-    },2000);    
+function createPost(post, callback) {
+  setTimeout(function() {
+    posts.push(post);
+    callback();
+  }, 2000);
 }
 
-function getPosts(){
-    setTimeout(function(){
-        let output='';
-        posts.forEach(function(post){
-          output +=`<li>${post.title}</li>`
-                      });
-        document.body.innerHTML=output;
-    },1000);
-    
+
+function getPosts() {
+  setTimeout(function() {
+    let output = '';
+    posts.forEach(function(post){
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
 }
 
-createPost({"title":"Post Three","body":"This is Post Three"},getPosts);
+createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
